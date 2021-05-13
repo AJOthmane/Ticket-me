696cd9b7a4e28233b1bf97bbbb3c860817fcb01e
omarirro <omarchbab@gmail.com>
2021-05-13 12:38:51 +0000

    creation de la page de statistiques

---
+++ app/src/main/java/ma/ensias/ticket_me/activities/StatsActivity.java
@@ -0,0 +1,16 @@
+package ma.ensias.ticket_me.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ma.ensias.ticket_me.R;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
